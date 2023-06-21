84b91dee632e4d477933e8dbd0cc7c8a93d777b7
David Lima <david.lima@ifal.edu.br>
2023-06-21 11:02:59 -0300

    Adicionando o arquivo tests.js

---
+++ 2o-bimestre/bimestral/tests.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+
+const {
+    createItem,
+    listItems,
+    getItemById,
+    updateItem,
+    deleteItem
+  } = require('./arquivo'); // Importe as funções do programa CRUD
+
+  
