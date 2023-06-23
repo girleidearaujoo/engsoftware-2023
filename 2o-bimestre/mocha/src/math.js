63d17e41838f3d8dfd337867972fc2a8b99c3712
David Lima <david.lima@ifal.edu.br>
2023-06-23 12:05:40 -0300

    Atualização pós aula

--- 2o-bimestre/mocha/src/math.js
+++ 2o-bimestre/mocha/src/math.js
@@ -2,4 +2,16 @@ function soma(a,b) {
     return a + b;
 }
 
-module.exports = {soma};
+function sub(a,b) {
+    return a - b;
+}
+
+function mult(a,b) {
+    return a * b;
+}
+
+function div(a,b) {
+    return a/b;
+}
+
+module.exports = {soma, sub, mult, div};
