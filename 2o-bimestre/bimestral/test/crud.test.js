ede6bc1c15b582350a0b8edd80d2aa43649bc444
mary0800 <115583142+girleidearaujoo@users.noreply.github.com>
2023-06-27 12:10:10 -0300

    Update crud.test.js

--- 2o-bimestre/bimestral/test/crud.test.js
+++ 2o-bimestre/bimestral/test/crud.test.js
@@ -22,6 +22,52 @@ describe("CRUD", function() {
   });
 
   describe("readItems()", function() {
+    it("Lê os intens da lista", function () {
+      let c = new CRUD();
+      c.createItem("Item 1");
+      c.createItem("Item 2");
+      c.createItem("Item 3");
 
+      let a = c.readItems()
+      assert.strictEqual(a.length, 3);
+    });
+  });
+
+  describe("readItem()", function() {
+    it("Lê 2 intens da lista", function () {
+      let c = new CRUD();
+      c.createItem("Item 1");
+      c.createItem("Item 2");
+      c.createItem("Item 3");
+
+      let a = c.readItem(0);
+      let b = c.readItem(1);
+      assert.strictEqual(a, c.readItem(0));
+      assert.strictEqual(b, c.readItem(1));
+    });  
+  });
+  describe("updateItem()", function() {
+    it("Atualiza os intens da lista", function () {
+      let c = new CRUD();
+      c.createItem("Iten 1");
+      c.createItem("Idem 2");
+      c.createItem("Item 3");
+
+      c.updateItem(0, "Item 1")
+      c.updateItem(1, "Item 2")
+      assert.strictEqual(c.readItem(0), "Item 1");
+      assert.strictEqual(c.readItem(1), "Item 2");
+    });
+  });
+  describe("deleteItem()", function() {
+    it("Deleta 2 intens da lista", function () {
+      let c = new CRUD();
+      c.createItem("Iten 1");
+      c.createItem("Idem 2");
+      c.createItem("Item 3");
+
+      assert.ok(c.deleteItem(0));
+      assert.ok(c.deleteItem(1));
+    });
   });
 });
